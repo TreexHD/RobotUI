1ec51d9994d7b168ec0c21c3ae804b54d2db2eed
Lino <lino.dino.waechter@gmail.com>
2024-10-04 00:17:49 +0200

    Flask first steps

--- robotui/main.py
+++ robotui/main.py
@@ -1,6 +1,14 @@
+import flask
+
 """
 this is the main file
 """
 
 class UI:
+
+    app = flask.Flask(__name__)
+
+    @app.route('/startseite')
+    def startseite(self):
+        return "Startseiten Test!"
     pass
