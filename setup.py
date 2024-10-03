4db4913bc6efd550c3ebc2b530c3adea66c650fd
TreexHD <67122079+TreexHD@users.noreply.github.com>
2024-10-04 00:01:46 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'this is the robot UI library'
+LONG_DESCRIPTION = 'A package that allows easy access to all functions of the robot via a UI in the web browser'
+
+# Setting up
+setup(
+    name="robotui",
+    version=VERSION,
+    author="TreexHD, LinoDino",
+    author_email="<no.mail>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'flask>=3.0.3'
+    ],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
